24366606682e1bdeba5ce69d6768154e69ab00c4
ksimpson <ksimpson@mindgruve.com>
2020-04-16 13:54:39 -0700

    starting to add idea of types

---
+++ src/Sharding/Types.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Sharding;
+
+class Types
+{
+
+    // FILE STORAGE
+    const FILE_IMAGE_JPG = 0;
+    const FILE_IMAGE_PNG = 1;
+    const FILE_IMAGE_SVG = 2;
+    const FILE_IMAGE_WEBP = 3;
+}
